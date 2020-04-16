1418854e67f7712bfbd605d49a9432fd691b2b4a
adeoye adela <adeoye.adela@gmail.com>
2020-04-17 00:09:48 +0100

    rps done

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
-
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  choices = ['rock', 'paper', 'scissors']
 
+  def possibilities(perm, n):
+    if n == 0: #base case
+      return perm
+    permutations = []
+    for arr in perm:
+      for move in choices:
+        permutations.append([*arr, move])
+    return possibilities(permutations, n - 1) # move towards base case
+  return possibilities([[]], n)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
